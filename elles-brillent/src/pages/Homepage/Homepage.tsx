84c247bcf5e72e50f6b7b82d1e88a5480993f5de
Alissone Neyret <alissone.neyret@gmail.com>
2024-10-30 11:46:27 +0100

    💄 update ui and add legal notices page

--- elles-brillent/src/pages/Homepage/Homepage.tsx
+++ elles-brillent/src/pages/Homepage/Homepage.tsx
@@ -15,29 +15,62 @@ export const Homepage = () => {
         <img className="homepage_header_stars" src={Stars} alt="" />
         <img className="homepage_header_background" src={Brush} alt="" />
       </header>
-      <section>
-        <div>
-          <h2>Elles brillent, c'est quoi ?</h2>
+
+      <main>
+        <section>
+          <div>
+            <h2>Elles brillent, c'est quoi ?</h2>
+            <p>
+              C'est un évènement en live et en ligne dans lequel je reçois 3
+              étoiles de la Tech (dev, designer, product manager, CTO etc.) pour
+              une interview de 25 min chacune.
+            </p>
+            <p>
+              Chaque étoile est invitée pour aborder un sujet précis. Dans Elles
+              brillent, pas de sujet technique, on parle de parcours, on met en
+              avant des réalisations, mais on aborde aussi des sujets peu
+              visibles.
+            </p>
+          </div>
+        </section>
+        <section>
+          <h2>Inscription</h2>
           <p>
-            Il s’agit d’un évènement en live et en ligne dans lequel je reçois 3 étoiles de la Tech (dev, designer, product manager, CTO etc.) pour
-            une interview de 25 min chacune. 
+            Pour être informé en avant première des invitées, des dates et bien
+            plus encore, inscrits-toi
           </p>
-          <p>
-            Chaque étoile est invitée pour aborder un sujet précis. Dans
-            Elles brillent, pas de sujet technique, on parle de parcours, on met
-            en avant des réalisations, mais on aborde aussi des sujets peu
-            visibles.
-          </p>
-        </div>
-      </section>
-      <section>
-        <h2>Inscription</h2>
-        <p>Pour être informé en avant première des invitées, de la date et de la plateforme où aura lieu l'évènement, inscrits-toi</p>
-        <div className="homepage_section_registration">
-          <label className="homepage_section_registration_label" htmlFor="email">Email</label>
-          <input className="homepage_section_registration_input" type="text" placeholder="Email" name="email" id="email" />
-        </div>
-      </section>
+          <form method="POST" action="https://gmail.us17.list-manage.com/subscribe/post">
+          <input type="hidden" name="u" value="8b40db9ee1e5114d385e41c54" />
+          <input type="hidden" name="id" value="540c5117ac" />
+            <div className="homepage_section_registration">
+              <label
+                className="homepage_section_registration_label"
+                htmlFor="MERGE0"
+              >
+                Email
+              </label>
+              <input
+                className="homepage_section_registration_input"
+                type="text"
+                placeholder="Email"
+                name="MERGE0"
+                id="MERGE0"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="homepage_section_registration_button"
+            >
+              S'inscrire
+            </button>
+          </form>
+        </section>
+      </main>
+
+      <footer>
+        <p>Elles brillent par Alissone Neyret - 2024 ©</p>
+      </footer>
     </div>
   );
 };
---
+++ elles-brillent/src/pages/LegalNotice/LegalNotices.tsx
@@ -0,0 +1,84 @@
+import React, { type FC } from "react";
+
+
+export const LegalNotices: FC = () => {
+	return (
+		<>
+			<h2>Directrice de la publication‍</h2>
+			<p>Alissone Neyret</p>
+
+			<h2>‍Contact‍</h2>‍ 
+      <p>Adresse électronique : alissone.neyret@gmail.com</p>
+
+		  <h2>Hébergement</h2> 
+      <p>Le Site est hébergé par la société Vercel INC.</p>
+
+      <h2>Contenu et utilisation des informations</h2> ‍
+      <p>L’ensemble des contenus, pages, scripts,
+			textes, graphismes, photographies, illustrations, présentations, icônes et
+			bases de données du Site et/ou des réseaux sociaux de Elles brillent sont
+			les propriétés exclusives de Alissone Neyre ou de tiers ayant autorisé
+			Elles brillent à les utiliser. Tous droits de reproduction ou de
+			représentation de ceux-ci sont strictement réservés. Par conséquent, toute
+			reproduction, représentation, utilisation, adaptation, modification,
+			incorporation, traduction, commercialisation, partielles ou intégrales, du
+			Site et/ou des réseaux sociaux de Elles brillent par quelque procédé et
+			sur quelque support que ce soit (papier, numérique, …) sont interdites,
+			sans l’autorisation écrite préalable de Alissone Neyret
+			sous peine de constituer un délit de contrefaçon de droit d’auteur et/ou
+			de dessins et modèles et/ou de marque. Les logos, marques, iconographies,
+			relatifs à toutes autres sociétés et produits cités sur le Site et/ou sur
+			les réseaux sociaux de Elles brillent sont la propriété respective de
+			leurs auteurs et/ou titulaires.</p>
+
+      <h2>Liens hypertextes‍</h2> 
+      <p>Lors de l’utilisation du
+			Site, Alissone Neyret se réserve le droit de procéder à des recommandations
+			de services de tiers ou d’éventuels partenaires. Dès lors, les
+			utilisateurs pourront avoir accès à différents liens les dirigeant vers
+			des sites tiers et/ou des sites d’éventuels partenaires. ‍ Alissone Neyret
+			ne sera en aucun cas responsable concernant : - le contenu des sites
+			référencés sur le Site et/ou sur les réseaux sociaux de Elles brillent, -
+			les informations ou contenus fournis par des tiers et/ou d’éventuels
+			partenaires. ‍ Elles brillent n’exerce aucun contrôle sur les liens
+			hypertextes et/ou autres formats promotionnels. Par conséquent, Elles brillent ne donne aucune garantie concernant notamment : - la véracité, la
+			qualité, la complétude et l’exhaustivité du contenu des sites internet
+			indexés ; - ou les difficultés d’accès et de fonctionnement des sites
+			internet indexés ;et ne pourra en aucun cas être tenue pour responsable
+			dans le cadre d’un litige survenant entre un site internet indexé sur le
+			Site et/ou sur les réseaux sociaux de Wonderful Tech et un des
+			utilisateurs, notamment en raison des pertes ou des dommages subis, les
+			exploitants des sites internet indexés étant seuls responsables du contenu
+			de leur site.</p>
+
+      <h2>Traitement des données à caractère personnel‍</h2> 
+      <p>Lors de la navigation via le formulaire d'inscription sur le Site, les utilisateurs sont
+			amenés à communiquer des données personnelles. À ce titre, les données
+			recueillies par Elles brillent sont utilisées pour permettre d'envoyer des emails d'information. ‍‍‍Conformément : au Règlement (UE) 2016/679 du
+			Parlement européen et du Conseil du 27 avril 2016 dit le « RGPD » relatif
+			à la protection des personnes physiques à l’égard du traitement des
+			données à caractère personnel et à la libre circulation de ces données ;
+			et à la loi n°78-17 dite « Informatique et Libertés » du 6 janvier 1978,
+			modifiée par la loi 2004-801 du 6 août 2004 relative à l’informatique, aux
+			fichiers et aux libertés ; les utilisateurs disposent d’un droit d’accès,
+			de modification, de rectification, de limitation, de portabilité et de
+			suppression des données les concernant. À cet effet, l’Utilisateur doit
+			s’adresser au service support par email à l’adresse suivante :
+			alissone.neyret@gmail.com</p>
+
+      <h2>Responsabilité</h2> 
+      <p>Elles brillent s’efforce
+			d’assurer, au mieux de ses possibilités, l'exactitude et l'actualité des
+			informations diffusées sur le Site. Elles brillent décline toute
+			responsabilité pour toute imprécision, inexactitude ou omission relative
+			aux informations disponibles sur le Site, pour les dommages résultant
+			d'une modification des informations figurant sur le Site causée par une
+			éventuelle manipulation frauduleuse effectuée par des tiers et pour tous
+			dommages, directs ou indirects, quelles qu'en soient les causes, origines,
+			nature ou conséquences, provoqués à raison de l'accès de quiconque au Site
+			ou de l'impossibilité d'y accéder, de même que l'utilisation du Site et/ou
+			du crédit accordé à une quelconque information provenant directement ou
+			indirectement de ce dernier.</p>
+		</>
+	);
+};
