75d924442630a55eb892764b7909e768c2bfe4fd
Alissone Neyret <alissone.neyret@gmail.com>
2024-11-04 17:28:15 +0100

    💄 update displaying title

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -14,7 +14,7 @@ export const Homepage = () => {
       <Header />
       <p className="hompegage_header_subtitle">
         Découvre les parcours inspirants de celles qui font briller la Tech
-        <span style={{fontWeight: "bold"}}> Mardi 26 Novembre à 18h30</span> en direct sur Twitch.
+        <span style={{fontWeight: "bold"}}> mardi 26 novembre à 18h30</span> en direct sur Twitch.
       </p>
       <main>
         <section className="homepage_what_section">
@@ -22,13 +22,13 @@ export const Homepage = () => {
             <h2>Qu'est-ce que <span className='--pink'>c'est</span> ?</h2>
             <p>
               Un évènement en live et en ligne dans lequel je reçois 3 étoiles
-              de la Tech (dev, designer, product manager, CTO etc.) pour une
-              interview de 25 min chacune.
+              de la Tech (dev, designer, product manager, CTO etc.) pour trois
+              interviews de 25 min chacunes.
             </p>
             <p>
               Chaque étoile est invitée pour aborder un sujet précis. On parle
               de parcours, on met en avant des réalisations et on aborde aussi
-              des sujets peu visibles.
+              des sujets peu visibles (santé mentale, inclusivité, etc.).
             </p>
           </div>
           <img
@@ -60,7 +60,7 @@ export const Homepage = () => {
             </p>
             <p>
               C'est au cours d'une de ces conversations que l'idée d'Elles
-              brillent a germé. Un événement où nous pourrions partager nos
+              brillent a germé. Un évènement où nous pourrions partager nos
               expériences, nos réussites et nos défis, les sujets qui nous
               tiennent à cœur et où chacune pourrait trouver la motivation et
               l'inspiration dont elle a besoin.
@@ -71,7 +71,7 @@ export const Homepage = () => {
         <section className="homepage_guests_section">
           <div>
             <h2>Les <span className='--pink'>invitées</span></h2>
-            <p>Pour la premières édition, les 3 étoiles invitées seront révélées le 13 Novembre.</p>
+            <p>Pour la première édition, les 3 étoiles invitées seront révélées le 13 Novembre.</p>
           </div>
           <img
             className="homepage_illustration --right"
@@ -93,7 +93,7 @@ export const Homepage = () => {
                 Apprendre : Découvre les réalisations de ces femmes et des
                 sujets peu visibles
               </li>
-              <li>Réseauter : Rejoigns une communauté de passionnés</li>
+              <li>Réseauter : Rejoins une communauté de passionnés</li>
               <li>
                 S'inspirer : Laisse-toi inspirer par ces parcours riches
               </li>
